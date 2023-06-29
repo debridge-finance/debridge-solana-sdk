d0481279532d7361aff6bcc5f9866189bde617c1
cyphersnake <cyphersnake@pm.me>
2023-06-29 11:40:33 +0400

    chore: add license
    Close #31

--- example-program/examples/send_by_program_sender.rs
+++ example-program/examples/send_by_program_sender.rs
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2023 debridge
+ *
+ * This file is part of debridge-solana-sdk.
+ *
+ * debridge-solana-sdk is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * debridge-solana-sdk is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with debridge-solana-sdk. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use std::str::FromStr;
 
 use anchor_lang::{InstructionData, ToAccountMetas};
